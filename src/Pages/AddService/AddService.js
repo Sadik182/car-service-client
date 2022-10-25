1b4b9c8a93e140b3bdf81dc7f202263d3c9cf5c8
sadik182 <sadikurrahman728@gmail.com>
2022-10-25 23:38:05 +0600

    Service added

--- src/Pages/AddService/AddService.js
+++ src/Pages/AddService/AddService.js
@@ -1,26 +1,40 @@
 import React from "react";
+import { useRef } from "react";
 import { Button, Form } from "react-bootstrap";
 
 const AddService = () => {
+  const titleRef = useRef('');
+  const priceRef = useRef('');
+  const descriptionRef = useRef('');
+  const imageRef = useRef('');
+  const handleAddService = e => {
+    const title = titleRef.current.value;
+    const price = priceRef.current.value;
+    const description = descriptionRef.current.value;
+    const image = imageRef.current.value;
+    const data = {title, price, description, image}
+    e.preventDefault()
+    console.log('Service Added', data);
+  }
   return (
     <div>
       <h1 className="text-center my-2"> Add New Service</h1>
-      <Form className="w-50 mx-auto mt-3 border p-4 rounded-3">
+      <Form onSubmit={handleAddService} className="w-50 mx-auto mt-3 border p-4 rounded-3">
         <Form.Group className="mb-3" controlId="formBasicTitle">
           <Form.Label>Service Title</Form.Label>
-          <Form.Control type="text" placeholder="Enter Service Title" />
+          <Form.Control ref={titleRef} type="text" placeholder="Enter Service Title" />
         </Form.Group>
         <Form.Group className="mb-3" controlId="formBasicPrice">
           <Form.Label>Service Price</Form.Label>
-          <Form.Control type="number" placeholder="Enter Service Price" />
+          <Form.Control ref={priceRef} type="number" placeholder="Enter Service Price" />
         </Form.Group>
         <Form.Group className="mb-3" controlId="formBasicDescription">
           <Form.Label>Service Description</Form.Label>
-          <Form.Control type="text" placeholder="Enter Service Description" />
+          <Form.Control ref={descriptionRef} type="text" placeholder="Enter Service Description" />
         </Form.Group>
         <Form.Group className="mb-3" controlId="formBasicImg">
           <Form.Label>Service Description</Form.Label>
-          <Form.Control type="file" placeholder="Enter Service Image" />
+          <Form.Control ref={imageRef} type="file" placeholder="Enter Service Image" />
         </Form.Group>
         <Button variant="primary" type="submit" className="d-block w-100">
           Add Service
